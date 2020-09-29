9470e30f5d093709e94c58add76027e93c926958
iobruno <bruno.ariev@gmail.com>
2020-09-29 19:39:51 -0300

    cleaning up some code

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,99 @@
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
 
-constexpr float FPS = 60.0f;
+#define screenWidth 1920
+#define screenHeight 1080
+#define bgColor al_map_rgb(0, 0, 0)
+#define refreshRate 1.0f/60
+
+ALLEGRO_DISPLAY *display;
+ALLEGRO_EVENT_QUEUE *event_queue;
+ALLEGRO_TIMER *timer;
+
+int startUp();
+void redrawScreen();
+void destroy();
 
 int main(int argc, char** argv) {
+    bool isRunning = true;
+    bool needsRedrawing = true;
 
-    ALLEGRO_DISPLAY *display = NULL;
-    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-    ALLEGRO_TIMER *timer = NULL;
-    bool running = true;
-    bool redraw = true;
-
-    if (!al_init()) {
-        fprintf(stderr, "Failed to initialize allegro.\n");
-        return 1;
-    }
-
-    // Initialize the timer
-    timer = al_create_timer(1.0f / FPS);
-    if (!timer) {
-        fprintf(stderr, "Failed to create timer.\n");
-        return 1;
-    }
-    // Create the display
-    display = al_create_display(1920, 1080);
-    if (!display) {
-        fprintf(stderr, "Failed to create display.\n");
-        return 1;
-    }
-
-    // Create the event queue
-    event_queue = al_create_event_queue();
-    if (!event_queue) {
-        fprintf(stderr, "Failed to create event queue.");
-        return 1;
-    }
-
-    // Register event sources
-    al_register_event_source(event_queue, al_get_display_event_source(display));
-    al_register_event_source(event_queue, al_get_timer_event_source(timer));
-
-    // Display a blank screen
-    al_clear_to_color(al_map_rgb(0, 0, 0));
-    al_flip_display();
-
-    // Start the timer
+    startUp();
+    redrawScreen();
     al_start_timer(timer);
 
-    while (running) {
+    while (isRunning) {
         ALLEGRO_EVENT event;
         ALLEGRO_TIMEOUT timeout;
-
-        // Initialize timeout
         al_init_timeout(&timeout, 0.06);
 
-        // Fetch the event (if one exists)
-        bool get_event = al_wait_for_event_until(event_queue, &event, &timeout);
-
-        // Handle the event
-        if (get_event) {
+        if (al_wait_for_event_until(event_queue, &event, &timeout)) {
             switch (event.type) {
                 case ALLEGRO_EVENT_TIMER:
-                    redraw = true;
+                    needsRedrawing = true;
                     break;
                 case ALLEGRO_EVENT_DISPLAY_CLOSE:
-                    running = false;
+                    isRunning = false;
                     break;
                 default:
                     fprintf(stderr, "Unsupported event received: %d\n", event.type);
                     break;
             }
         }
-        // Check if we need to redraw
-        if (redraw && al_is_event_queue_empty(event_queue)) {
-            // Redraw
-            al_clear_to_color(al_map_rgb(0, 0, 0));
-            al_flip_display();
-            redraw = false;
+
+        // Check if we need to needsRedrawing
+        if (needsRedrawing && al_is_event_queue_empty(event_queue)) {
+            redrawScreen();
+            needsRedrawing = false;
         }
     }
 
-    // Clean up
-    al_destroy_display(display);
-    al_destroy_event_queue(event_queue);
+    destroy();
+    return 0;
+}
 
+/**
+ * Intializestimer, display and eventQueue
+ * @return int
+ */
+int startUp() {
+
+    if (!al_init()) {
+        al_show_native_message_box(nullptr , "Pinpoint++", nullptr,
+                                   "Could not initialize Allegro", nullptr, NULL);
+        return 1;
+    }
+
+    display = al_create_display(screenWidth, screenHeight);
+    timer = al_create_timer(refreshRate);
+    event_queue = al_create_event_queue();
+
+    if (!timer or !display or !event_queue) {
+        al_show_native_message_box(nullptr, "Pinpoint++", nullptr,
+                                   "Error Initialing Timer, Display or Event Queue", nullptr, NULL);
+        return 1;
+    }
+    else {
+        al_register_event_source(event_queue, al_get_display_event_source(display));
+        al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    }
 
     return 0;
+}
+
+/**
+ * Draws/Redraws screen bgColor
+ */
+void redrawScreen() {
+    al_clear_to_color(bgColor);
+    al_flip_display();
+}
+
+/**
+ * Finishes the Application
+ */
+void destroy() {
+    al_destroy_display(display);
+    al_destroy_event_queue(event_queue);
 }
